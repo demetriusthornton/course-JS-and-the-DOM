906d6a729f8912b25843b555e1fad22b04a266fb
Demetrius D. <demetriusthornton@gmail.com>
2024-10-21 19:51:42 -0400

    update 2

--- app.js
+++ app.js
@@ -2,3 +2,6 @@
 let element = document.getElementById("Title");
 // element.innerHTML = "This is a new title";
 document.title = "This is a new title";
+
+const programName = "$0";
+programName.innerHTML = "<em>Everything</em> Your Need to Know About Data";
