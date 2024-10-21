4a8c9ca6309364a9b6c92107c9af872394e460e1
Demetrius D. <demetriusthornton@gmail.com>
2024-10-21 19:51:08 -0400

    update 2

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 //trying to change the title of the page
 let element = document.getElementById("Title");
-element.innerHTML = "This is a new title";
+// element.innerHTML = "This is a new title";
+document.title = "This is a new title";
