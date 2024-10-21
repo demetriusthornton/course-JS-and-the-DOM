e35cfbd50febb96220a7e8e655e7929e3ecf5c8f
Demetrius D. <demetriusthornton@gmail.com>
2024-10-21 19:55:53 -0400

    update three

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 //trying to change the title of the page
 let element = document.getElementById("Title");
-// element.innerHTML = "This is a new title";
 document.title = "This is a new title";
 
 const programName = "$0";
-programName.innerHTML = "<em>Everything</em> Your Need to Know About Data";
+programName.innerText = "Some Things You Need to Know about Data";
