0872e9a3350d9943521cc769d056cae98ab822cc
Demetrius D. <demetriusthornton@gmail.com>
2024-10-24 00:30:07 -0400

    CLASS WORK

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 //trying to change the title of the page
-let element = document.getElementById("Title");
-document.title = "This is a new title";
+// let element = document.getElementById("Title");
+// document.title = "This is a new title";
 
-const programName = "$0";
-programName.Textcontent = "Some Things You Need to Know about Data";
+// const programName = $0;
+// programName.textContent = "Some Things You Need to Know about Data";
+
+// const container = document.createElement("span");
+// container.textContent = ", right now!";
+
+const heading = $0;
+heading.style.color = "orange";
